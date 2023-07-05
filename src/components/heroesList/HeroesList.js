f905f075e6e790f55232085e32e7179cfe145b41
gunther <korzhovdh@gmail.com>
2023-07-05 17:08:38 +0200

    createSlice and createAsyncThunk added

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,10 +2,10 @@ import {useHttp} from '../../hooks/http.hook';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import { createSelector } from 'reselect'
+import { createSelector } from '@reduxjs/toolkit'
 
-import { fetchHeroes } from '../../actions';
-import {heroDeleted} from './heroesSlice';
+
+import {fetchHeroes,heroDeleted} from './heroesSlice'; 
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
@@ -38,7 +38,7 @@ const HeroesList = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
   
 
         // eslint-disable-next-line
